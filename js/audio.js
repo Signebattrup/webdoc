15b26a240fdba522e2deac32f6ea08423be03ee3
Signe <battrup@live.com>
2019-02-18 11:20:22 +0100

    ny mappe

---
+++ js/audio.js
@@ -0,0 +1,40 @@
+/**
+ * fil: audio.js
+ * purpose: play audio when the containing div is visible
+ **/
+window.addEventListener("scroll", function() { // detect scroll event
+
+  var mySound = document.getElementById("myaudio"); // select audio id
+  var lyd = document.getElementById("lyd");
+  
+// Below: start and stop audio 
+  if (elFllVsbl(lyd)) { // if the #lyd div is visible then
+    if (!(mySound.curentTime > 0)) {
+      mySound.play(); // play audio
+    }
+  } else {
+    mySound.pause();
+    mySound.currentTime = 0; // rewind the sound
+  }
+})
+
+// Below: start and stop audio 
+  if (elFllVsbl(lyd2)) { // if the #lyd div is visible then
+    if (!(mySound2.curentTime > 0)) {
+      mySound2.play(); // play audio
+    }
+  } else {
+    mySound2.pause();
+    mySound2.currentTime = 0; // rewind the sound
+  }
+})
+
+// this function will check whether a tag is visible
+function elFllVsbl(el) {
+  return (el.getBoundingClientRect().top >= 0 && el.getBoundingClientRect().bottom < window.innerHeight);
+}
+
+/*
+NB: the audio / video tags will allways return true,
+solution: place the tag in a div and test for the div in stead
+*/
